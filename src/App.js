1428dbb56cae4d71809bf79424b7ca3af9f5aabd
Andrei Bezmen <andryshabez26@gmail.com>
2020-09-29 00:24:36 +0300

    feat: add  photo container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/header';
+import PhotoContainer from './components/photo-container/photo-container';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="divide-line"></div>
+      <PhotoContainer />
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
+      <div className="divide-line"></div>
       <div className="divide-line"></div>
     </div>
   );
