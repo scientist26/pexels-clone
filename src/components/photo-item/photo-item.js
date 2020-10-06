6209e63a27dd6f82c05cfb557745c6df4696fa58
Andrei Bezmen <andryshabez26@gmail.com>
2020-10-06 14:34:16 +0300

    feat: add like/dislike icon

--- src/components/photo-item/photo-item.js
+++ src/components/photo-item/photo-item.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
 import './photo-item.css';
 
-const PhotoItem = ({ photo, onNextPage }) => {
-  const { src, photographer, photographer_url } = photo;
-  return (
-    <div className="photo-item">
-      <a className="photo-item__link" href="#/">
-        <img className="photo-item__img" src={src.medium} alt="img" onClick={onNextPage}></img>
-      </a>
-      <div className="photo-item__info">
-        <a
-          className="photo-item__author"
-          rel="noopener noreferrer"
-          target="_blank"
-          href={photographer_url}
-        >
-          <span className="photo-item__author-name">{photographer}</span>
+class PhotoItem extends Component {
+  state = {
+    selected: false,
+  };
+
+  addLike = () => {
+    if (this.state.selected === false) {
+      this.setState({
+        selected: true,
+      });
+    } else {
+      this.setState({
+        selected: false,
+      });
+    }
+  };
+
+  render() {
+    const { photo, onNextPage } = this.props;
+    const { selected } = this.state;
+    const htmlColor = selected ? 'red' : 'white';
+    return (
+      <div className="photo-item">
+        <a className="photo-item__link" href="#/">
+          <img
+            className="photo-item__img"
+            src={photo.src.medium}
+            alt="img"
+            onClick={onNextPage}
+          ></img>
         </a>
-        <button className="photo-item__author-like"></button>
+        <div className="photo-item__info">
+          <a
+            className="photo-item__author"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={photo.photographer_url}
+          >
+            <span className="photo-item__author-name">{photo.photographer}</span>
+          </a>
+          <button className="photo-item__author-like">
+            <FavoriteIcon
+              htmlColor={htmlColor}
+              className="photo-item__author-like-svg"
+              onClick={this.addLike}
+            />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PhotoItem;
